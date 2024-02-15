8cc6edd745904cb8fc5d7732adc07da0b8cfbd7b
aminaliaqat04 <aminaliaqat04@gmail.com>
2024-02-15 17:55:12 +0500

    env test added and project name update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col justify-center px-5 lg:w-2/3 mx-auto space-y-5">
-      <p className="font-bold text-2xl lg:text-4xl">Welcome to Hunting Coder</p>
+      <p className="font-bold text-2xl lg:text-4xl">Welcome to Diary Craft</p>
       <p className="text-sm lg:text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, aut voluptatibus maiores optio magni saepe, dolore eos facilis, nam doloremque vel esse dignissimos veritatis quis. Odio magnam totam sunt asperiores.</p>
       <p className="text-sm lg:text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, aut voluptatibus maiores optio magni saepe, dolore eos facilis, nam doloremque vel esse dignissimos veritatis quis. Odio magnam totam sunt asperiores.</p>
     </main>
